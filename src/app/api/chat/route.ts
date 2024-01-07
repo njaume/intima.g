f4c788759d131ecd26f034566d9b237385ba5cdf
Nahuel Jaume <nahueljaume@gmail.com>
2024-01-07 16:05:39 -0300

    open ai api disabled

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,16 +2,16 @@ import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
  
 // Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
+/* const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-});
+}); */
  
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
  
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  /* const { messages } = await req.json();
  
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
@@ -22,5 +22,6 @@ export async function POST(req: Request) {
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
   // Respond with the stream
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream); */
+  return {}
 }
