148c03f8df52216323654ccaf7b6093d0668bd44
Nahuel Jaume <nahueljaume@gmail.com>
2024-01-07 16:05:58 -0300

    open ai api disabled

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
-import OpenAI from 'openai';
+/* import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
- 
+ */ 
 // Create an OpenAI API client (that's edge friendly!)
 /* const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
