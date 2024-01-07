a3f2b12ff1e4a890734806903cbf63be08901dcf
Nahuel Jaume <nahueljaume@gmail.com>
2024-01-07 16:01:23 -0300

    open ai api disabled

--- src/frontend/components/business/Chat/index.tsx
+++ src/frontend/components/business/Chat/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useChat } from "ai/react";
+//import { useChat } from "ai/react";
 import ChatMessagesList from "./ChatMessagesList";
 import { Button } from "../../ui/button";
 import { Input } from "../../ui/input";
@@ -14,18 +14,18 @@ const MESSAGE_LIST = [
   }
 ]
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  //const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <form className="pb-8 w-full h-screen" onSubmit={handleSubmit}>
+    <form className="pb-8 w-full h-screen" /* onSubmit={handleSubmit} */>
       <ChatMessagesList messages={MESSAGE_LIST} />
       <div className="w-full fixed bottom-10 left-0 px-10">
           <div className="flex gap-5">
             <Input
               className="input-ghost-primary input-block input text-white"
               placeholder="Primary"
-              value={input}
-              onChange={handleInputChange}
+              //value={input}
+             // onChange={handleInputChange}
             />
             <Button className="btn btn-primary" type="submit">
               Send
